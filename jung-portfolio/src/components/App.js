8cbd5e837d2cbc663e27bf8a81beb06e6deacff2
jung <jung@saramin.co.kr>
2018-07-24 15:19:19 +0900

    https://velopert.com/957

--- jung-portfolio/src/components/App.js
+++ jung-portfolio/src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Header from './layout/Header';
+import Content from './layout/Content';
 
 class App extends React.Component {
     render(){
 
         return (
-            <h1>Hello React. I'm PaPa</h1>
+            <div>
+                <Header title={ this.props.headerTitle }/>
+                <Content title={ this.props.contentTitle } body={ this.props.contentBody }/>
+            </div>
         );
     }
 }
-
+ App.defaultProps = {
+    headerTitle: 'Default header',
+    contentTitle: 'Default contentTitle',
+    contentBody: 'Default contentBody'
+ }
 export default App;
